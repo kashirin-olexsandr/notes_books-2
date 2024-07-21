47a96bf5bdc8a09e827ac6057c3739176fcaac03
kashirin-olexsandr <114315489+kashirin-olexsandr@users.noreply.github.com>
2024-07-21 10:48:55 +0300

    Initial commit

---
+++ src/index.js
@@ -0,0 +1,8 @@
+/**
+ * Розмітка картки задачі
+ * <li class="task-list-item">
+ *     <button class="task-list-item-btn">Delete</button>
+ *     <h3>Заголовок</h3>
+ *     <p>Текст</p>
+ * </li>
+ */
